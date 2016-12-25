0f0caddd2883481ee4a2727820b6e37136818b62
Daphne <anastasiyasofr@yandex.ru>
2016-12-25 18:38:16 +0300

    google drive sync added, really hope it works

--- app/src/main/java/ru/spbau/mit/placenotifier/GoogleDriveSync.java
+++ app/src/main/java/ru/spbau/mit/placenotifier/GoogleDriveSync.java
@@ -1,44 +1,59 @@
 package ru.spbau.mit.placenotifier;
 
+import android.Manifest;
 import android.accounts.AccountManager;
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.widget.DrawerLayout;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.ProgressBar;
 
-import com.google.android.gms.common.AccountPicker;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
-import com.google.android.gms.drive.query.Filters;
-import com.google.android.gms.drive.query.Query;
-import com.google.android.gms.drive.query.SearchableField;
+import com.google.android.gms.auth.GoogleAuthUtil;
+import com.google.android.gms.auth.UserRecoverableAuthException;
+import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.api.client.googleapis.extensions.android.gms.auth.UserRecoverableAuthIOException;
+import com.google.api.client.http.FileContent;
+import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.client.util.ExponentialBackOff;
 import com.google.api.services.drive.Drive;
 import com.google.api.services.drive.DriveScopes;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.services.drive.model.File;
+import com.google.api.services.drive.model.FileList;
 
-import pub.devrel.easypermissions.AfterPermissionGranted;
-import pub.devrel.easypermissions.EasyPermissions;
-import com.google.android.gms.auth.GoogleAuthUtil;
-
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-/**
- * Created by daphne on 14.12.16.
- */
+import static android.content.pm.PackageManager.PERMISSION_GRANTED;
+
 
 public class GoogleDriveSync extends Activity {
     private static final String FILENAME = "placenotifier";
     private GoogleAccountCredential mCredential;
-    private static final String[] SCOPES = { DriveScopes.DRIVE };
-
+    private static final String[] SCOPES = {DriveScopes.DRIVE};
+    ProgressBar progress;
     private static final int CHOOSE_ACCOUNT = 1;
+    private static final int DOWNLOAD = 2;
+    private static final int UPLOAD = 3;
+    AlarmManager manager;
+    Button download;
+    Button upload;
+    AccountManager accountManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +63,13 @@ public class GoogleDriveSync extends Activity {
                 getApplicationContext(), Arrays.asList(SCOPES))
                 .setBackOff(new ExponentialBackOff());
         chooseAccount();
+        progress = (ProgressBar) findViewById(R.id.update_GD_progress);
+        manager = new AlarmManager(this);
+        download = (Button)findViewById(R.id.download);
+        download.setOnClickListener(v -> update(DOWNLOAD));
+        upload = (Button)findViewById(R.id.upload);
+        upload.setOnClickListener(v -> update(UPLOAD));
+        accountManager = AccountManager.get(this);
     }
 
     protected void chooseAccount() {
@@ -63,23 +85,159 @@ public class GoogleDriveSync extends Activity {
         }
     }
 
-    protected void update() {}
-
-    private boolean fileAlreadyExist(Activity main) {
+    private boolean requestPermission() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.GET_ACCOUNTS) == PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this,
+                        Manifest.permission.READ_CONTACTS) == PERMISSION_GRANTED)
+            return true;
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.GET_ACCOUNTS, Manifest.permission.READ_CONTACTS}, 0);
         return false;
     }
 
-    private void createFile() {
+    @TargetApi(Build.VERSION_CODES.N)
+    protected void update(int request) {
+        AsyncTask<Void, Void, String> task = new AsyncTask<Void, Void, String>() {
+            @Override
+            protected String doInBackground(Void... params) {
+                try {
+                    if (!requestPermission())
+                        return "none";
+                    String mScope = "oauth2:"+ DriveScopes.DRIVE;
+                    final HttpTransport mTransport = AndroidHttp.newCompatibleTransport();
+                    final JsonFactory mJsonFactory = GsonFactory.getDefaultInstance();
+                    Drive driveService = new Drive.Builder(mTransport, mJsonFactory, mCredential).build();
+                    GoogleAuthUtil.getToken(getBaseContext(), mCredential.getSelectedAccountName(), mScope);
+                    if (!fileAlreadyExist(driveService))
+                        createFile(driveService);
+                    File content = getFile(driveService);
+                    ObjectInputStream stream = downloadContent(driveService, content);
+                    List<Alarm> fromDrive = readFromStream(stream);
+                    List<Alarm> fromDatabase = manager.getAlarms();
+                    List<Alarm> all = new ArrayList<>();
+                    if (request == DOWNLOAD) {
+                        all.addAll(fromDatabase);
+                        all.removeAll(fromDrive);
+                        all.addAll(fromDrive);
+                        for (Alarm alarm : all) {
+                            manager.updateAlarm(alarm);
+                        }
+                    } else {
+                        all.addAll(fromDrive);
+                        all.removeAll(fromDatabase);
+                        all.addAll(fromDatabase);
+                        driveService.files().delete(content.getId()).execute();
+                        uploadContent(all, driveService);
+                    }
+                    return "done";
+                }
+                catch (UserRecoverableAuthException e) {
+                    startActivityForResult(e.getIntent(), 0);
+                    return "none";
+                }
+                catch (UserRecoverableAuthIOException e) {
+                    startActivityForResult(e.getIntent(), 0);
+                    return "none";
+                }
+                catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
 
+            @Override
+            protected void onPostExecute(String token) {
+                Log.i("", "Updated:" + token);
+            }
+        };
+        task.execute();
     }
 
-
-    public List<Alarm> getAlarmsFromDrive(Activity main) {
-        if (!fileAlreadyExist(main)) {
-            createFile();
+    private boolean fileAlreadyExist(Drive drive) {
+        List<File> res = new ArrayList<>();
+        try {
+            FileList files = drive.files().list().setQ("title='place-notifier'").execute();
+            res.addAll(files.getItems());
         }
-        return null;
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return !res.isEmpty();
     }
 
+    private void createFile(Drive drive) {
+        File fileMetadata = new File();
+        fileMetadata.setTitle("place-notifier");
+        try {
+            drive.files().insert(fileMetadata).setFields("id").execute();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private File getFile(Drive drive) {
+        List<File> res = new ArrayList<>();
+        try {
+            FileList files = drive.files().list().setQ("title='place-notifier'").execute();
+            res.addAll(files.getItems());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return res.get(0);
+    }
+
+    private ObjectInputStream downloadContent(Drive drive, File file) {
+        if (file.getDownloadUrl() != null && file.getDownloadUrl().length() > 0) {
+            try {
+                InputStream input = drive.files().get(file.getId()).executeMediaAsInputStream();
+                if (input.available() > 0)
+                    return new ObjectInputStream(drive.files().get(file.getId()).executeMediaAsInputStream());
+                else
+                    return null;
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            return null;
+        }
+    }
+
+    private List<Alarm> readFromStream(ObjectInputStream stream) {
+        List<Alarm> res = new ArrayList<>();
+        try {
+            if (stream == null)
+                return res;
+            while (stream.available() > 0) {
+                res.add((Alarm) stream.readObject());
+            }
+        } catch (IOException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return res;
+    }
+
+    private void uploadContent(List <Alarm> list, Drive drive) {
+        try {
+            java.io.File path = Environment.getExternalStoragePublicDirectory(
+                    Environment.DIRECTORY_MOVIES);
+            java.io.File file = new java.io.File(path, "/" + "place-notifier-tmp");
+            FileOutputStream out = new FileOutputStream(file);
+            ObjectOutputStream outputStream = new ObjectOutputStream(out);
+            for (Alarm alarm : list) {
+                outputStream.writeObject(alarm);
+            }
+            File fileMetadata = new File();
+            fileMetadata.setTitle("place-notifier");
+            FileContent mediaContent = new FileContent("", file);
+            drive.files().insert(fileMetadata, mediaContent)
+                    .setFields("id")
+                    .execute();
+
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 
 }
---
+++ app/src/main/java/ru/spbau/mit/placenotifier/ServiceReminder.java
@@ -0,0 +1,104 @@
+package ru.spbau.mit.placenotifier;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.NotificationCompat;
+import android.os.Handler;
+
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import static android.content.Context.LOCATION_SERVICE;
+import static android.content.Context.NOTIFICATION_SERVICE;
+import static android.content.pm.PackageManager.PERMISSION_GRANTED;
+
+class ServiceReminder {
+
+    private static final long MILLISEC_IN_MINUTE = 60000;
+    private Timer reminder;
+    private TimerTask task;
+    private Handler handler;
+    private AlarmManager manager;
+
+    ServiceReminder(Activity main) {
+        manager = new AlarmManager(main);
+        reminder = new Timer();
+        handler = new Handler();
+        task = new TimerTask() {
+            public void run() {
+                handler.post(() -> {
+                    try {
+                        List<Alarm> result = manager.getAlarms();
+                        sendNotification(main, result);
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+            }
+        };
+        reminder.schedule(task, 0, MILLISEC_IN_MINUTE);
+    }
+
+    private boolean requestPermission(Activity main) {
+        if (ContextCompat.checkSelfPermission(main,
+                    Manifest.permission.ACCESS_FINE_LOCATION) == PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(main,
+                        Manifest.permission.ACCESS_COARSE_LOCATION) == PERMISSION_GRANTED)
+            return true;
+        ActivityCompat.requestPermissions(main,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION}, 0);
+        return false;
+    }
+
+    private void sendNotification(Activity main, List<Alarm> result) throws SecurityException {
+        if (result == null)
+            return;
+        if (!requestPermission(main))
+            return;
+        LocationManager locationManager = (LocationManager) main.getSystemService(LOCATION_SERVICE);
+        Location location = null;
+        if (locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER))
+            location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        else
+            location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+        Long time = System.currentTimeMillis();
+
+        for (Alarm notif : result) {
+            if (!notif.getPlacePredicate().apply(location) ||
+                    !notif.getTimePredicate().apply(time))
+                continue;
+            NotificationCompat.Builder builder = (NotificationCompat.Builder) new NotificationCompat.Builder(main)
+                    .setSmallIcon(R.drawable.alarm)
+                    .setContentTitle(notif.getName())
+                    .setContentText(notif.getComment());
+            Notification not = builder.build();
+            not.defaults = Notification.DEFAULT_ALL;
+            Intent resultIntent = new Intent(main, main.getClass());
+            PendingIntent resultPendingIntent =
+                    PendingIntent.getActivity(
+                            main,
+                            notif.getIdentifier().hashCode(),
+                            resultIntent,
+                            PendingIntent.FLAG_UPDATE_CURRENT
+                    );
+            builder.setContentIntent(resultPendingIntent);
+            NotificationManager mNotifyMgr =
+                    (NotificationManager) main.getSystemService(NOTIFICATION_SERVICE);
+            mNotifyMgr.notify(1, not);
+        }
+
+    }
+
+
+}
